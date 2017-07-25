db38678b54724bef1c7b2862fbe32a4cdc16ee30
jlsicajan <sicajancoy@gmail.com>
2017-07-24 23:28:27 -0600

    configuracion para cabeceras

--- app.js
+++ app.js
@@ -11,6 +11,24 @@ var api = require('./routes/favorito');
 
 app.use(body_parser.urlencoded({extended:false}));
 app.use(body_parser.json());
+
+//aqui vamos a crear un middleware para evitar errores con el PUT y el DELETE
+app.use(function(request, response, next){
+//vamos a configurar los headers, postman ya te los configura automaticamente
+    //le decimos que cualquiera puede hacer peticiones a nuestro api rest, si quisieramos
+    //que solo se hicieran peticiones desde un dominio concreto, lo ponemos en el asterisco
+    request.header('Access-Control-Allow-Origin', '*')
+    //ahora configuramos los headers que le pueden llegar
+    request.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+    //vamos a configurar los metodos http que pueden utilizarse en el api
+    request.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    //allow
+    request.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next(); //para que se salga de esta funcion
+    //esto nos da la posibilidad de utlilizar put delete en nuestras peticiones
+});
+
 app.use('/api', api);
 
 module.exports = app; //de esta forma podemos exporta este objeto
